5e9e45d06b7fff3f89ad5287411d5aad8e739760
Daniel Campos <d.camposmartins99@gmail.com>
2021-07-10 02:25:46 -0300

    Chart style set

--- app/src/main/java/pdm/moodmanager/HistoryActivity.kt
+++ app/src/main/java/pdm/moodmanager/HistoryActivity.kt
@@ -23,25 +23,22 @@ class HistoryActivity: AppCompatActivity() {
         setContentView(R.layout.history_activity)
 
         linelist = ArrayList()
-        linelist.add(Entry(10f,100f))
-        linelist.add(Entry(20f,300f))
-        linelist.add(Entry(30f,200f))
-        linelist.add(Entry(40f,600f))
-        linelist.add(Entry(50f,500f))
-        linelist.add(Entry(50f,900f))
+        linelist.add(Entry(01f,2f))
+        linelist.add(Entry(02f,-1f))
+        linelist.add(Entry(03f,-3f))
+        linelist.add(Entry(04f,-1f))
+        linelist.add(Entry(05f,4f))
+        linelist.add(Entry(06f,2f))
 
         lineDataSet = LineDataSet(linelist,"Count")
         lineData = LineData(lineDataSet)
         lineChart.data = lineData
-        lineDataSet.color = Color.BLACK
-//        lineDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
+//        lineDataSet.color = Color.BLACK
+        lineDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
         lineDataSet.valueTextColor= Color.BLUE
         lineDataSet.valueTextSize = 20f
         lineDataSet.setDrawFilled(true)
 
-
-//        var chart:LineChart = findViewById(R.id.lineChart)
-//        var data:LineData =
 
     }
 }
