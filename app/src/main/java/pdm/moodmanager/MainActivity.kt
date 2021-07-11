14f7f224598289f52b97229cc6d1a239de6b8a5c
Daniel Campos <d.camposmartins99@gmail.com>
2021-07-11 01:20:47 -0300

    Quick Refact

--- app/src/main/java/pdm/moodmanager/MainActivity.kt
+++ app/src/main/java/pdm/moodmanager/MainActivity.kt
@@ -27,36 +27,9 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.home_activity)
 //        setContentView(R.layout.signup_activity)
 //        setContentView(R.layout.activity_main)
-//        test()
 
-//        lateinit var p: UserModel
-//        val body = HashMap<String, Any>()
-//        body.put("email","daniel@dan.com")
-//        body.put("username","danielfolgado")
-//        body.put("password","123")
-//
-//        userRequest.register(body, object :APIListener{
-//            override fun onSuccess(model: Any) {
-//
-//            }
-//
-//            override fun onFailure(str: String) {
-//                val s = ""
-//            }
-//
-//        })
     }
 
-//    fun test(){
-//        var user:User = User("Dan","segredo1010")
-//        println("Users: ${user}")
-//        user.ToPositive()
-//        user.ToPositive()
-//        println("Users: ${user}")
-//        user.ToNegative()
-//        user.ToNegative()
-//        println("Users: ${user}")
-//    }
 
     fun OnSigninSubmit(view: View){
 
